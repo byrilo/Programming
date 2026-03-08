3fe57729774bd7e6c9406139e2ab422e368a7a55
byrilo <evs.dima2207@gmail.com>
2026-03-09 01:58:41 +0700

    feature(Model): Добавлены свойства, автосвойства, конструкторы для класса Discipline.

--- Programming/Programming/Model/Discipline.cs
+++ Programming/Programming/Model/Discipline.cs
@@ -12,5 +12,74 @@ namespace Programming.Model
         private int _grade;
         private string _teacher;
         private int _disciplineHours;
+
+        public string DisciplineName // Свойcтво названия дисциплины
+        {
+            get
+            {
+                return _disciplineName;
+            }
+            set
+            {
+                if (value.Length < 0)
+                {
+                    MessageBox.Show("Ошибка. Введите название дисциплины.");
+                    _disciplineName = value;
+                }
+            }
+        }
+        public int Grade // Свойcтво оценки
+        {
+            get
+            {
+                return _grade;
+            }
+            set
+            {
+                if (value < 0 && value > 5)
+                {
+                    MessageBox.Show("Ошибка. Введите корректное значение оценки.");
+                    _grade = value;
+                }
+            }
+        }
+        public string Teacher // Свойcтво имени преподавателя
+        {
+            get
+            {
+                return _teacher;
+            }
+            set
+            {
+                if (value.Length < 0)
+                {
+                    MessageBox.Show("Ошибка. Введите имя преподавателя.");
+                    _teacher = value;
+                }
+            }
+        }
+        public int DisciplineHours // Свойcтво оценки
+        {
+            get
+            {
+                return _disciplineHours;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    MessageBox.Show("Ошибка. Введите корректное значение часов дисциплины.");
+                    _disciplineHours = value;
+                }
+            }
+        }
+        public Discipline() { } // Конструкторы
+        public Discipline(string disciplinename, int grade, string teacher, int disciplinehours)
+        {
+            DisciplineName = disciplinename;
+            Grade = grade;
+            Teacher = teacher;
+            DisciplineHours = disciplinehours;
+        }
     }
 }
