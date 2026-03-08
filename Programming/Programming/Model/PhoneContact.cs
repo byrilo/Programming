f7482efbac41f5a411a5aff85f2159cfa2de6405
byrilo <evs.dima2207@gmail.com>
2026-03-09 01:22:46 +0700

    feature(Model): Добавлены свойства, автосвойства, конмтрукторы для класса PhoneContact.

--- Programming/Programming/Model/PhoneContact.cs
+++ Programming/Programming/Model/PhoneContact.cs
@@ -8,10 +8,51 @@ namespace Programming.Model
 {
     public class PhoneContact
     {
-        private string ContactName;
-        private string ContactSurname;
-        private int PhoneNumber;
-        private string Adress;
+        private string _contactName;
+        private string _contactSurname;
+        private int _phoneNumber;
+        private string _adress;
 
+        public string ContactName // Свойcтво имени контакта
+        {
+            get
+            {
+                return _contactName;
+            }
+            set
+            {
+                if (value.Length < 0)
+                {
+                    MessageBox.Show("Ошибка. Введите имя.");
+                    _contactName = value;
+                }
+            }
+        }
+        public int PhoneNumber // Свойcтво номера телефона
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                if (value.ToString().Length != 11)
+                {
+                    MessageBox.Show("Ошибка. Введите верный номер.");
+                    _phoneNumber = value;
+                }
+            }
+        }
+        public string Adress { get; set; } // Автосвойства
+        public string ContactSurname { get; set; }
+
+        public PhoneContact() { } // Конструкторы
+        public PhoneContact(string contactname, int phonenumber, string adress, string contactsurname)
+        {
+            ContactName = contactname;
+            PhoneNumber = phonenumber;
+            Adress = adress;
+            ContactSurname = contactname;
+        }
     }
 }
