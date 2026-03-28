c832dd3a06eab841af3a576e3d2751a0039e2f2b
byrilo <evs.dima2207@gmail.com>
2026-03-29 01:59:28 +0700

    feature (Model): Добавлен класс точки Point2D.

---
+++ Programming/Programming/Model/Point2D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public class Point2D
+    {
+        // Свойства доступны для чтения извне, но могут быть установлены только внутри класса
+        private int _x;
+        public int X
+        {
+            get => _x;
+            private set
+            {
+                //Validator.AssertOnPositiveValue(value, nameof(X));
+                _x = value;
+            }
+        }
+        private int _y;
+        public int Y
+        {
+            get => _y;
+            private set
+            {
+                _y = value;
+            }
+        }
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
