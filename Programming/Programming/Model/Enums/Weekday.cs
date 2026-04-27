6fb8547a076d46de183ae07fe10b5b32f87225e4
byrilo <evs.dima2207@gmail.com>
2026-04-27 23:07:13 +0700

    docs(Enums): Автодокументирование для класса Weekday.

--- Programming/Programming/Model/Enums/Weekday.cs
+++ Programming/Programming/Model/Enums/Weekday.cs
@@ -6,14 +6,44 @@ using System.Threading.Tasks;
 
 namespace Programming.Model
 {
+    /// <summary>
+    /// Перечисление, представляющее дни недели.
+    /// </summary>
     internal enum Weekday
     {
+        /// <summary>
+        /// Понедельник.
+        /// </summary>
         Monday,
+
+        /// <summary>
+        /// Вторник.
+        /// </summary>
         Tuesday,
+
+        /// <summary>
+        /// Среда.
+        /// </summary>
         Wednesday,
+
+        /// <summary>
+        /// Четверг.
+        /// </summary>
         Thursday,
+
+        /// <summary>
+        /// Пятница.
+        /// </summary>
         Friday,
+
+        /// <summary>
+        /// Суббота.
+        /// </summary>
         Saturday,
+
+        /// <summary>
+        /// Воскресенье.
+        /// </summary>
         Sunday
     }
 }
