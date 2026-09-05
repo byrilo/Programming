1ddf2891878b44c574c61acf96069276a498cbfd
byrilo <evs.dima2207@gmail.com>
2026-09-06 02:13:50 +0700

    feature(Services): Добавлен класс ItemFactory для будущей генерации случайных значений предметов

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Services/ItemFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectOrientedPractics.Model;
+
+namespace ObjectOrientedPractics.Services
+{
+    public static class ItemFactory
+    {
+        private static readonly string[] _names = 
+        {
+            "Беспроводные наушники",
+            "Смартфон",
+            "Ноутбук",
+            "Умные часы",
+            "Портативная колонка",
+            "Внешний аккумулятор",
+            "Клавиатура механическая",
+            "Веб-камера"
+        };
+        private static readonly string[] _infos =
+        {
+            "Качественный товар с гарантией производителя 12 месяцев.",
+            "Компактный и лёгкий, подходит для повседневного использования.",
+            "Высокая производительность и стильный дизайн.",
+            "Долгое время автономной работы от одного заряда.",
+            "Совместим с большинством современных устройств."
+        };
+
+        private static readonly Random _random = new Random();
+
+        public static Item GetRandomItem()
+        {
+            string name = _names[_random.Next(_names.Length)];
+            string info = _infos[_random.Next(_infos.Length)];
+            double cost = _random.Next(0, 100000);
+            return new Item(name, info, cost);
+        }
+    }
+}
