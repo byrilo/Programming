55c53c0443b923b304f66c59d29620a601659bef
byrilo <evs.dima2207@gmail.com>
2026-09-19 02:05:55 +0700

    feature(Controls): Добавлен файл пользовательского элемента с вёрсткой окна ввода адреса доставки.
    docs(Adress): Добавлены xml-комментарии для класса адреса, свойств, конструкторов.

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/Address.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectOrientedPractics.Services;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class Address
+    {
+        /// <summary>
+        /// Индекс получателя.
+        /// </summary>
+        private int _index;
+        /// <summary>
+        /// Страна получателя.
+        /// </summary>
+        private string _country = string.Empty;
+        /// <summary>
+        /// Город получателя.
+        /// </summary>
+        private string _city = string.Empty;
+        /// <summary>
+        /// Улица получателя.
+        /// </summary>
+        private string _street = string.Empty;
+        /// <summary>
+        /// Подьезд получателя.
+        /// </summary>
+        private string _building = string.Empty;
+        /// <summary>
+        /// Квартира получателя.
+        /// </summary>
+        private string _apartment = string.Empty;
+
+        /// <summary>
+        /// Свойство для получения и установки индекса. Индекс должен быть шестизначным числом.
+        /// </summary>
+        public int Index
+        {
+            get { return _index; }
+            set
+            {
+                if (value < 100000 || value > 999999)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Индекс должен быть шестизначным числом.");
+                }
+                _index = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство для получения и установки страны. Страна должна быть не длиннее 50 символов.
+        /// </summary>
+        public string Country
+        {
+            get { return _country; }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50, nameof(Country));
+                _country = value;
+            }
+        }
+
+        /// <summary>
+        /// Город получателя. Свойство для получения и установки города. Город должен быть не длиннее 50 символов.
+        /// </summary>
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50, nameof(City));
+                _city = value;
+            }
+        }
+
+        /// <summary>
+        /// Улица получателя. Свойство для получения и установки улицы. Улица должна быть не длиннее 100 символов.
+        /// </summary>
+        public string Street
+        {
+            get { return _street; }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 100, nameof(Street));
+                _street = value;
+            }
+        }
+
+        /// <summary>
+        /// Подьезд получателя. Свойство для получения и установки подъезда. Подьезд должен быть не длиннее 10 символов.
+        /// </summary>
+        public string Building
+        {
+            get { return _building; }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10, nameof(Building));
+                _building = value;
+            }
+        }
+
+        /// <summary>
+        /// Квартира получателя. Свойство для получения и установки квартиры. Квартира должна быть не длиннее 10 символов.
+        /// </summary>
+        public string Apartment
+        {
+            get { return _apartment; }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10, nameof(Apartment));
+                _apartment = value;
+            }
+        }
+        /// <summary>
+        /// Конструктор класса Address, который инициализирует новый экземпляр адреса с пустыми значениями.
+        /// </summary>
+        public Address()
+        {
+        }
+        /// <summary>
+        /// Конструктор класса Address, который инициализирует новый экземпляр адреса с заданными значениями.
+        /// </summary>
+        /// <param name="index">Индекс адреса.</param>
+        /// <param name="country">Страна адреса.</param>
+        /// <param name="city">Город адреса.</param>
+        /// <param name="street">Улица адреса.</param>
+        /// <param name="building">Номер здания адреса.</param>
+        /// <param name="apartment">Номер квартиры адреса.=</param>
+        public Address(int index, string country, string city, string street, string building, string apartment)
+        {
+            Index = index;
+            Country = country;
+            City = city;
+            Street = street;
+            Building = building;
+            Apartment = apartment;
+        }
+    }
+}
---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ObjectOrientedPractics.View.Controls
+{
+    partial class AddressControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            _addressMainLabel = new Label();
+            _postIndexLabel = new Label();
+            _countryLabel = new Label();
+            _streetLabel = new Label();
+            _buildingLabel = new Label();
+            _cityLabel = new Label();
+            _apartamentLabel = new Label();
+            _postIndexTextBox = new TextBox();
+            _countryTextBox = new TextBox();
+            _streetTextBox = new TextBox();
+            _cityTextBox = new TextBox();
+            _buildingTextBox = new TextBox();
+            _apartamentTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // _addressMainLabel
+            // 
+            _addressMainLabel.AutoSize = true;
+            _addressMainLabel.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            _addressMainLabel.Location = new Point(20, 27);
+            _addressMainLabel.Name = "_addressMainLabel";
+            _addressMainLabel.Size = new Size(135, 20);
+            _addressMainLabel.TabIndex = 0;
+            _addressMainLabel.Text = "Delivery Address";
+            _addressMainLabel.Click += label1_Click;
+            // 
+            // _postIndexLabel
+            // 
+            _postIndexLabel.AutoSize = true;
+            _postIndexLabel.Location = new Point(20, 78);
+            _postIndexLabel.Name = "_postIndexLabel";
+            _postIndexLabel.Size = new Size(79, 20);
+            _postIndexLabel.TabIndex = 1;
+            _postIndexLabel.Text = "Post index:";
+            // 
+            // _countryLabel
+            // 
+            _countryLabel.AutoSize = true;
+            _countryLabel.Location = new Point(20, 121);
+            _countryLabel.Name = "_countryLabel";
+            _countryLabel.Size = new Size(63, 20);
+            _countryLabel.TabIndex = 2;
+            _countryLabel.Text = "Country:";
+            // 
+            // _streetLabel
+            // 
+            _streetLabel.AutoSize = true;
+            _streetLabel.Location = new Point(20, 166);
+            _streetLabel.Name = "_streetLabel";
+            _streetLabel.Size = new Size(51, 20);
+            _streetLabel.TabIndex = 3;
+            _streetLabel.Text = "Street:";
+            // 
+            // _buildingLabel
+            // 
+            _buildingLabel.AutoSize = true;
+            _buildingLabel.Location = new Point(20, 214);
+            _buildingLabel.Name = "_buildingLabel";
+            _buildingLabel.Size = new Size(67, 20);
+            _buildingLabel.TabIndex = 4;
+            _buildingLabel.Text = "Building:";
+            // 
+            // _cityLabel
+            // 
+            _cityLabel.AutoSize = true;
+            _cityLabel.Location = new Point(301, 125);
+            _cityLabel.Name = "_cityLabel";
+            _cityLabel.Size = new Size(37, 20);
+            _cityLabel.TabIndex = 5;
+            _cityLabel.Text = "City:";
+            // 
+            // _apartamentLabel
+            // 
+            _apartamentLabel.AutoSize = true;
+            _apartamentLabel.Location = new Point(247, 217);
+            _apartamentLabel.Name = "_apartamentLabel";
+            _apartamentLabel.Size = new Size(91, 20);
+            _apartamentLabel.TabIndex = 6;
+            _apartamentLabel.Text = "Apartament:";
+            // 
+            // _postIndexTextBox
+            // 
+            _postIndexTextBox.Location = new Point(105, 75);
+            _postIndexTextBox.Name = "_postIndexTextBox";
+            _postIndexTextBox.Size = new Size(125, 27);
+            _postIndexTextBox.TabIndex = 7;
+            // 
+            // _countryTextBox
+            // 
+            _countryTextBox.Location = new Point(105, 121);
+            _countryTextBox.Name = "_countryTextBox";
+            _countryTextBox.Size = new Size(190, 27);
+            _countryTextBox.TabIndex = 8;
+            // 
+            // _streetTextBox
+            // 
+            _streetTextBox.Location = new Point(105, 166);
+            _streetTextBox.Name = "_streetTextBox";
+            _streetTextBox.Size = new Size(412, 27);
+            _streetTextBox.TabIndex = 9;
+            // 
+            // _cityTextBox
+            // 
+            _cityTextBox.Location = new Point(344, 125);
+            _cityTextBox.Name = "_cityTextBox";
+            _cityTextBox.Size = new Size(173, 27);
+            _cityTextBox.TabIndex = 10;
+            // 
+            // _buildingTextBox
+            // 
+            _buildingTextBox.Location = new Point(105, 214);
+            _buildingTextBox.Name = "_buildingTextBox";
+            _buildingTextBox.Size = new Size(125, 27);
+            _buildingTextBox.TabIndex = 11;
+            // 
+            // _apartamentTextBox
+            // 
+            _apartamentTextBox.Location = new Point(344, 217);
+            _apartamentTextBox.Name = "_apartamentTextBox";
+            _apartamentTextBox.Size = new Size(125, 27);
+            _apartamentTextBox.TabIndex = 12;
+            // 
+            // AddressControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(_apartamentTextBox);
+            Controls.Add(_buildingTextBox);
+            Controls.Add(_cityTextBox);
+            Controls.Add(_streetTextBox);
+            Controls.Add(_countryTextBox);
+            Controls.Add(_postIndexTextBox);
+            Controls.Add(_apartamentLabel);
+            Controls.Add(_cityLabel);
+            Controls.Add(_buildingLabel);
+            Controls.Add(_streetLabel);
+            Controls.Add(_countryLabel);
+            Controls.Add(_postIndexLabel);
+            Controls.Add(_addressMainLabel);
+            Name = "AddressControl";
+            Size = new Size(550, 261);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label _addressMainLabel;
+        private Label _postIndexLabel;
+        private Label _countryLabel;
+        private Label _streetLabel;
+        private Label _buildingLabel;
+        private Label _cityLabel;
+        private Label _apartamentLabel;
+        private TextBox _postIndexTextBox;
+        private TextBox _countryTextBox;
+        private TextBox _streetTextBox;
+        private TextBox _cityTextBox;
+        private TextBox _buildingTextBox;
+        private TextBox _apartamentTextBox;
+    }
+}
