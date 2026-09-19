41543d4d1e083d9b93f7e449e019769753ac2ab5
byrilo <evs.dima2207@gmail.com>
2026-09-19 13:46:44 +0700

    feature(AddressControl): Исправлена работа TextBox.
    docs(AddressControl): Добавлены описывающие работу кода xml-комментарии.

--- src/ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.cs
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressControl.cs
@@ -14,11 +14,17 @@ namespace ObjectOrientedPractics.View.Controls
 {
     public partial class AddressControl : UserControl
     {
+        /// <summary>
+        /// Создание пустого адреса для заполнения полей.
+        /// </summary>
         private Address _address = new Address();
         public AddressControl()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Функция отображения адреса в полях ввода.
+        /// </summary>
         private void ShowAddress()
         {
             _postIndexTextBox.Text = _address.Index.ToString();
@@ -28,13 +34,18 @@ namespace ObjectOrientedPractics.View.Controls
             _buildingTextBox.Text = _address.Building;
             _apartmentTextBox.Text = _address.Apartment;
         }
-
+        /// <summary>
+        /// Валидация поля ввода страны. 
+        /// </summary>
+        /// <param name="sender">Обьект события</param>
+        /// <param name="e">Аргументы события</param>
         private void _countryTextBox_TextChanged(object sender, EventArgs e)
         {
             try
             {
                 _address.Country = _countryTextBox.Text;
                 _countryTextBox.BackColor = SystemColors.Window;
+                _addressErrorProvider.SetError(_countryTextBox, string.Empty);
             }
             catch (ArgumentException ex)
             {
@@ -42,13 +53,18 @@ namespace ObjectOrientedPractics.View.Controls
                 _addressErrorProvider.SetError(_countryTextBox, ex.Message);
             }
         }
-
+        /// <summary>
+        /// Валидация поля ввода улицы.
+        /// </summary>
+        /// <param name="sender">Обьект события</param>
+        /// <param name="e">Аргументы события</param>
         private void _streetTextBox_TextChanged(object sender, EventArgs e)
         {
             try
             {
                 _address.Street = _streetTextBox.Text;
                 _streetTextBox.BackColor = SystemColors.Window;
+                _addressErrorProvider.SetError(_streetTextBox, string.Empty);
             }
             catch (ArgumentException ex)
             {
@@ -56,13 +72,18 @@ namespace ObjectOrientedPractics.View.Controls
                 _addressErrorProvider.SetError(_streetTextBox, ex.Message);
             }
         }
-
+        /// <summary>
+        /// Валидация поля ввода подъезда.
+        /// </summary>
+        /// <param name="sender">Обьект события</param>
+        /// <param name="e">Аргументы события</param>
         private void _buildingTextBox_TextChanged(object sender, EventArgs e)
         {
             try
             {
                 _address.Building = _buildingTextBox.Text;
                 _buildingTextBox.BackColor = SystemColors.Window;
+                _addressErrorProvider.SetError(_buildingTextBox, string.Empty);
             }
             catch (ArgumentException ex)
             {
@@ -70,13 +91,18 @@ namespace ObjectOrientedPractics.View.Controls
                 _addressErrorProvider.SetError(_buildingTextBox, ex.Message);
             }
         }
-
+        /// <summary>
+        /// Валидация поля ввода города.
+        /// </summary>
+        /// <param name="sender">Обьект события</param>
+        /// <param name="e">Аргументы события</param>
         private void _cityTextBox_TextChanged(object sender, EventArgs e)
         {
             try
             {
                 _address.City = _cityTextBox.Text;
                 _cityTextBox.BackColor = SystemColors.Window;
+                _addressErrorProvider.SetError(_cityTextBox, string.Empty);
             }
             catch (ArgumentException ex)
             {
@@ -84,13 +110,18 @@ namespace ObjectOrientedPractics.View.Controls
                 _addressErrorProvider.SetError(_cityTextBox, ex.Message);
             }
         }
-
+        /// <summary>
+        /// Валидация поля ввода квартиры.
+        /// </summary>
+        /// <param name="sender">Обьект события</param>
+        /// <param name="e">Аргументы события</param>
         private void _apartmentTextBox_TextChanged(object sender, EventArgs e)
         {
             try
             {
                 _address.Apartment = _apartmentTextBox.Text;
                 _apartmentTextBox.BackColor = SystemColors.Window;
+                _addressErrorProvider.SetError(_apartmentTextBox, string.Empty);
             }
             catch (ArgumentException ex)
             {
@@ -98,7 +129,11 @@ namespace ObjectOrientedPractics.View.Controls
                 _addressErrorProvider.SetError(_apartmentTextBox, ex.Message);
             }
         }
-
+        /// <summary>
+        /// Валидация поля ввода индекса.
+        /// </summary>
+        /// <param name="sender">Обьект события</param>
+        /// <param name="e">Аргументы события</param>
         private void _postIndexTextBox_TextChanged(object sender, EventArgs e)
         {
             if (int.TryParse(_postIndexTextBox.Text, out int index))
@@ -120,10 +155,10 @@ namespace ObjectOrientedPractics.View.Controls
                 _postIndexTextBox.BackColor = Color.Red;
                 _addressErrorProvider.SetError(_postIndexTextBox, "Индекс должен быть числом.");
             }
-        }
-
-        [System.ComponentModel.Browsable(false)]
-        [System.ComponentModel.DesignerSerializationVisibility(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        }       
+        /// <summary>
+        /// Получает или задает адрес.
+        /// </summary>
         public Address Address
         {
             get { return _address; }
